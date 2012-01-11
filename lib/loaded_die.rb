fafa45f92fefe68bdefb943821466b70d0ff60cb
Kaspar Schiess <kaspar.schiess@absurd.li>
2012-01-11 08:30:38 +0100

    + Comments and a license

--- lib/loaded_die.rb
+++ lib/loaded_die.rb
@@ -1,16 +1,35 @@
+# A loaded die that takes a probability map as constructor argument and will
+# generate die rolls every time you call #roll. Die sides are numbered 0-n,
+# where n is the size of the probability map. 
+#
+# Example: 
+#   # A three sided die with probabilities 1/6, 2/6, 3/6
+#   die = LoadedDie.new(1,2,3)
+#   die.roll    # => one of {0, 1, 2}
+#
+# This is Voses Alias Method from an excellent description of the various
+# algorithms by Keith Schwarz. It has a setup cost of O(n), a cost for each
+# roll of O(1) and uses O(n) memory. I recommend reading "Darts, Dice and
+# Coins: Sampling from a Discrete Distribution" by Keith Schwarz.
+#
 class LoadedDie
+  # Returns the number of sides this die has. 
   attr_reader :sides
+  # The original probability map. 
   attr_reader :probabilities
   attr_reader :normalized_probabilities
   
+  # Sets up a die with the given probabilities. No need to have a probability
+  # map that sums to 1, the code will normalize the numbers and provide you
+  # with #normalized_probabilities. 
+  #
   def initialize(*probabilities)
+    init(*probabilities)
+  end
+  def init(*probabilities) # :nodoc: 
     @probabilities = probabilities
     @sides = probabilities.size
     
-    init(*@probabilities)
-  end
-  
-  def init(*probabilities)
     @alias = Array.new(sides)
     @prob  = Array.new(sides)
     
@@ -54,6 +73,10 @@ class LoadedDie
     end
   end
   
+  # Rolls the die once and returns a number in the range 0...sides. Numbers
+  # will be distributed relative to the probabilities given in
+  # #normalized_probabilities.
+  #
   def roll
     # First roll a die with homogenous distribution: 
     n = rand(sides)
